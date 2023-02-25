f956de933189c923b2a6805e4d724215b5ca35c9
Kels <116579655+kwhis@users.noreply.github.com>
2023-02-24 23:19:46 -0500

    Add 8 ball shaking effect and hide answer

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     var magic = {};
-    magic.answers = ["Yes", "No", "Maybe so", "Of course", "It is certain", "Absolutely", "Never", "Never ever", "Ask again later", "In your dreams", "How dare you even ask"];
+    magic.possibleAnswers = ["Yes", "No", "Maybe so", "Of course", "It is certain", "Absolutely", "Never", "Never ever", "Ask again later", "In your dreams", "How dare you even ask"];
+
+    $(".answer").hide();
+
+    magic.askQuestion = function(question){
+        $("img").effect("shake");
+    }
 
 });

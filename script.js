f33445bd877ad36bcd26b894273a602acdc17a9c
Kels <116579655+kwhis@users.noreply.github.com>
2023-02-24 23:06:33 -0500

    Create script and add list of answers

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    var magic = {};
+    magic.answers = ["Yes", "No", "Maybe so", "Of course", "It is certain", "Absolutely", "Never", "Never ever", "Ask again later", "In your dreams", "How dare you even ask"];
+
+});

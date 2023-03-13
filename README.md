65584e8f3c15a14ddfb2f721f68727f6cb94169d
Kels <116579655+kwhis@users.noreply.github.com>
2023-03-13 12:59:28 -0400

    Reformat with Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # magic8ball
+
 Magic 8 Ball game using Math.random to produce a randomized answer. Ask the Magic 8 Ball a question and get an answer.
--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-$(document).ready(function(){
-    var magic = {};
-    magic.possibleAnswers = ["Yes", "No", "Maybe so", "Of course", "It is certain", "Absolutely", "Never", "Never ever", "Ask again later", "Nope"];
+$(document).ready(function () {
+  var magic = {};
+  magic.possibleAnswers = [
+    "Yes",
+    "No",
+    "Maybe so",
+    "Of course",
+    "It is certain",
+    "Absolutely",
+    "Never",
+    "Never ever",
+    "Ask again later",
+    "Nope",
+  ];
 
+  $(".answer").hide();
+
+  magic.askQuestion = function (question) {
+    $(".8ballgif").effect("shake");
+    $(".8ballgif").attr("src", "images/magic8ball.png");
+    $(".answer").fadeIn(4000);
+
+    var randomNum = Math.random();
+    var randomNumForAnswers = randomNum * this.possibleAnswers.length;
+    var randomIndex = Math.floor(randomNumForAnswers);
+    var magicAnswer = this.possibleAnswers[randomIndex];
+
+    $(".answer").text(magicAnswer);
+
+    console.log(question);
+    console.log(magicAnswer);
+  };
+
+  var onClick = function () {
     $(".answer").hide();
+    $(".8ballgif").attr("src", "images/magic8ball.gif");
 
-    magic.askQuestion = function(question){
-        $(".8ballgif").effect("shake");
-        $(".8ballgif").attr("src", "images/magic8ball.png");
-        $(".answer").fadeIn(4000);
+    setTimeout(function () {
+      var question = prompt("What would you like to ask the Magic 8 Ball?");
+      magic.askQuestion(question);
+    }, 500);
+  };
 
-        var randomNum = Math.random();
-        var randomNumForAnswers = randomNum * this.possibleAnswers.length;
-        var randomIndex = Math.floor(randomNumForAnswers);
-        var magicAnswer = this.possibleAnswers[randomIndex];
-
-        $(".answer").text(magicAnswer);
-
-        console.log(question);
-        console.log(magicAnswer);
-    };
-
-    var onClick = function(){
-        $(".answer").hide();
-        $(".8ballgif").attr("src", "images/magic8ball.gif");
-
-        setTimeout(function(){
-            var question = prompt("What would you like to ask the Magic 8 Ball?");
-            magic.askQuestion(question);
-        },500);    
-    };
-
-    $("button").click(onClick);
+  $("button").click(onClick);
 });
